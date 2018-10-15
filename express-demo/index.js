29204ad5271f2aabc329bfd8337da73827e4ffdb
Rafael Camarda <rf.camarda@gmail.com>
2018-10-15 18:19:45 -0300

    add new project modularization

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,84 +1,19 @@
-const Joi = require('joi');
+const debug = require('debug')('app:startup');
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
+const courses = require('./routes/courses');
+const home = require('./routes/home');
 
+app.set('view engine', 'pug');
 app.use(express.json());
+app.use('/api/courses', courses);
+app.use('/', home);
 
-const courses = [
-  {id: 1, name: 'course1'},
-  {id: 2, name: 'course2'},
-  {id: 3, name: 'course3'},
-]
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.get('/api/courses', (req, res) => {
-  res.send(courses);
-});
-
-app.get('/api/courses/:id', (req, res) =>  {
-  let course = courses.find(c => c.id === parseInt(req.params.id));
-
-  if (!course) {
-    return res.status(404).send('The course with the given ID was not found');
-  }
-
-  res.send(course);
-});
-
-app.post('/api/courses', (req, res) => {
-  const { error } = validateCourse(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name
-  }
-
-  courses.push(course);
-  res.send(course);
-});
-
-app.put('/api/courses/:id', (req, res) => {
-  let course = courses.find(c => c.id === parseInt(req.params.id));
-
-  if (!course) {
-    return res.status(404).send('The course with the given ID was not found');
-  }
-
-  const { error } = validateCourse(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  course.name = req.body.name;
-  res.send(course);
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-  let course = courses.find(c => c.id === parseInt(req.params.id));
-
-  if (!course) {
-    return res.status(404).send('The course with the given ID was not found');
-  }
-
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-
-  res.send(course);
-})
-
-function validateCourse(course){
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-
-  return Joi.validate(course, schema);
-};
+if(app.get('env') === 'development'){
+  app.use(morgan('tiny'));
+  debug('Morgan enabled...');
+}
 
 const port = process.env.PORT || 3000; 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
